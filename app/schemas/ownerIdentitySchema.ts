39d802e7ca6ce97aac2dab0450d19796a7fe6ebc
sankalpa-acharya <sankalpaacharya6609@gmail.com>
2024-08-28 12:19:23 +0530

    owner form zod

--- app/schemas/ownerIdentitySchema.ts
+++ app/schemas/ownerIdentitySchema.ts
@@ -1,5 +1,22 @@
 import { z } from "zod"
 
-export const ownerIdentitySchema = z.object({
 
+const isClient = typeof window !== "undefined";
+
+const fileSchema = isClient
+    ? z.instanceof(FileList).refine(files => files.length > 0, {
+        message: "this field is required"
+    }).refine(files => Array.from(files).every(file => ["image/jpeg", "image/png"].includes(file.type)), {
+        message: "Invalid file type. Only JPEG and PNG files are allowed."
+    })
+    : z.any();
+
+export const ownerIdentitySchema = z.object({
+    hostelBlock: z.string({ required_error: "Hostel Block is required" }).max(2, "Block name can't be more than 2 characters").min(1, "Hostel Block is required"),
+    hostelRoom: z.string({ required_error: "Hostel Room is required" }).max(3, "Not a valid room number").min(1, "Room no is required"),
+    collegeIDPhoto: fileSchema,
+    hostelIDPhoto: fileSchema,
+    profilePhoto: fileSchema,
+    QRPhoto: fileSchema,
+    messageToRenter: z.string({ required_error: "Message is required" }).max(500, "Block name can't be more than 2 characters").min(1, "Hostel Block is required")
 })
