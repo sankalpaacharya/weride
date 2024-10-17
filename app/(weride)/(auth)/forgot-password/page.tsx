ca53bd4804e8cec16fb6918fb09867f9382c1304
sankalpaacharya <sankalpaacharya01@gmail.com>
2024-10-17 11:41:09 +0530

    chore: changed func name from layout to forgotpassword

--- app/(weride)/(auth)/forgot-password/page.tsx
+++ app/(weride)/(auth)/forgot-password/page.tsx
@@ -14,7 +14,7 @@ import { Button } from "@/components/ui/button";
 
 type Props = {};
 
-export default function layout({}: Props) {
+export default function ForgotPassword({}: Props) {
   const [email, setEmail] = useState<string>("");
 
   return (
