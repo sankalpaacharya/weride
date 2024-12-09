9e43d3e09e805cb358666339362dc64aaaba3bfa
sankalpaacharya <sankalpaacharya01@gmail.com>
2024-12-09 22:44:03 +0530

    fix: linter fixes

--- app/schemas/checkOutSchema.ts
+++ app/schemas/checkOutSchema.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 export const checkOutSchema = z.object({
-  hour:z.string(),
-  kilometer:z.string(),
-  location:z.string()
-})
+  hour: z.string(),
+  kilometer: z.string(),
+  location: z.string(),
+});
 
-export type TcheckOutSchema = z.infer<typeof checkOutSchema>
+export type TcheckOutSchema = z.infer<typeof checkOutSchema>;
