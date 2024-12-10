5523d366fd487dbf6e11f75a02e1be23e1233e5e
sankalpaacharya <sankalpaacharya01@gmail.com>
2024-12-10 20:58:02 +0530

    fix(hydration): hydration error fix

--- components/rentalmodal/index.tsx
+++ components/rentalmodal/index.tsx
@@ -45,101 +45,94 @@ export default function RentalModal({ children }: RentalModal) {
         {selectedTab === "terms" ? (
           <div>
             <DialogTitle>Terms and Conditions</DialogTitle>
-            <DialogDescription>
-              <ScrollArea className="h-[500px] rounded-md p-4">
-                <ul className="space-y-2 mt-5">
-                  <span className="text-md font-bold text-gray-800">
-                    Vehicle Rental Agreement
-                  </span>
-                  <li className="">
-                    {" "}
-                    <span className="font-bold">3.1. Rental Duration:</span> You
-                    as a rider agree to rent the vehicle for the duration agreed
-                    upon with the vehicle owner. Any extension of time will
-                    result in late fees charge levied on the rider{" "}
-                    <Link className="underline" href={"/"}>
-                      (read late fees policy)
-                    </Link>
-                  </li>
-                  <li className="">
-                    {" "}
-                    <span className="font-bold">3.2. Rental Fees:</span> You
-                    agree to pay the hourly rental fee as set by the vehicle
-                    owner. The rental fee must be paid in full before the rental
-                    period begins.
-                  </li>
-                  <li className="">
-                    {" "}
-                    <span className="font-bold">
-                      3.3. Petrol Refill Requirement:
-                    </span>{" "}
-                    Riders must refill the vehicle to the petrol level specified
-                    by the owner(pre-decided) before returning the vehicle.
-                    Failure to do so will result in additional charges.
-                  </li>
-                </ul>
-                <ul className="space-y-2 mt-5">
-                  <span className="text-md font-bold text-gray-800">
-                    Vehicle Condition
-                  </span>
-                  <li className="">
-                    {" "}
-                    <span className="font-bold">5.1. Inspection:</span> Both the
-                    Rider and the Owner must inspect the vehicle before the
-                    rental period begins, noting any pre-existing damage.
-                  </li>
-                  <li className="">
-                    {" "}
-                    <span className="font-bold">5.2. Damage or Loss:</span>{" "}
-                    Riders are responsible for any damage or loss that occurs
-                    during the rental period. In case of any damage, the rider
-                    agrees to compensate the vehicle owner for the cost of
-                    repairs or replacement.
-                  </li>
-                  <li className="">
-                    {" "}
-                    <span className="font-bold">5.3. Cleanliness:</span> The
-                    vehicle must be returned in a clean condition. A cleaning
-                    fee of Rs 200 will be charged if the vehicle is returned
-                    excessively dirty.
-                  </li>
-                </ul>
-                <ul className="space-y-2 mt-5 list-disc">
-                  <span className="text-md font-bold text-gray-800">
-                    Reckless Driving Policy
-                  </span>
-                  <span>
-                    Reckless driving is strictly prohibited while using any
-                    vehicle rented through our platform. While weride does not
-                    have direct control over the actions of users, any reports
-                    or evidence of reckless driving may result in immediate
-                    termination of the rental agreement, forfeiture of any
-                    deposits, and a permanent ban from our platform.
-                    Additionally, renters are fully responsible for any legal
-                    consequences, fines, penalties, or damages resulting from
-                    reckless driving
-                    <Link className="underline" href="/">
-                      {" "}
-                      (read more).
-                    </Link>
-                  </span>
-                </ul>
-                <div className="flex items-center my-5 space-x-2">
-                  <Checkbox
-                    id="terms"
-                    checked={isTosAccepted}
-                    onCheckedChange={(checked) =>
-                      setIsTosAccepted(checked as boolean)
-                    }
-                  />
-                  <label
-                    htmlFor="terms"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    I have read all the terms and conditions and I accept it.
-                  </label>
-                </div>
-              </ScrollArea>
+            <DialogDescription asChild>
+              <div>
+                <ScrollArea className="h-[500px] rounded-md p-4">
+                  <div className="space-y-2 mt-5">
+                    <span className="text-md font-bold text-gray-800">
+                      Vehicle Rental Agreement
+                    </span>
+                    <div>
+                      <span className="font-bold">3.1. Rental Duration:</span>{" "}
+                      You as a rider agree to rent the vehicle for the duration
+                      agreed upon with the vehicle owner. Any extension of time
+                      will result in late fees charge levied on the rider{" "}
+                      <Link className="underline" href={"/"}>
+                        (read late fees policy)
+                      </Link>
+                    </div>
+                    <div>
+                      <span className="font-bold">3.2. Rental Fees:</span> You
+                      agree to pay the hourly rental fee as set by the vehicle
+                      owner. The rental fee must be paid in full before the
+                      rental period begins.
+                    </div>
+                    <div>
+                      <span className="font-bold">
+                        3.3. Petrol Refill Requirement:
+                      </span>{" "}
+                      Riders must refill the vehicle to the petrol level
+                      specified by the owner (pre-decided) before returning the
+                      vehicle. Failure to do so will result in additional
+                      charges.
+                    </div>
+                  </div>
+                  <div className="space-y-2 mt-5">
+                    <span className="text-md font-bold text-gray-800">
+                      Vehicle Condition
+                    </span>
+                    <div>
+                      <span className="font-bold">5.1. Inspection:</span> Both
+                      the Rider and the Owner must inspect the vehicle before
+                      the rental period begins, noting any pre-existing damage.
+                    </div>
+                    <div>
+                      <span className="font-bold">5.2. Damage or Loss:</span>{" "}
+                      Riders are responsible for any damage or loss that occurs
+                      during the rental period. In case of any damage, the rider
+                      agrees to compensate the vehicle owner for the cost of
+                      repairs or replacement.
+                    </div>
+                    <div>
+                      <span className="font-bold">5.3. Cleanliness:</span> The
+                      vehicle must be returned in a clean condition. A cleaning
+                      fee of Rs 200 will be charged if the vehicle is returned
+                      excessively dirty.
+                    </div>
+                  </div>
+                  <div className="space-y-2 mt-5">
+                    <span className="text-md font-bold text-gray-800">
+                      Reckless Driving Policy
+                    </span>
+                    <div>
+                      Reckless driving is strictly prohibited while using any
+                      vehicle rented through our platform. While weride does not
+                      have direct control over the actions of users, any reports
+                      or evidence of reckless driving may result in immediate
+                      termination of the rental agreement, forfeiture of any
+                      deposits, and a permanent ban from our platform.{" "}
+                      <Link className="underline" href="/">
+                        (read more).
+                      </Link>
+                    </div>
+                  </div>
+                  <div className="flex items-center my-5 space-x-2">
+                    <Checkbox
+                      id="terms"
+                      checked={isTosAccepted}
+                      onCheckedChange={(checked) =>
+                        setIsTosAccepted(checked as boolean)
+                      }
+                    />
+                    <label
+                      htmlFor="terms"
+                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                      I have read all the terms and conditions and I accept it.
+                    </label>
+                  </div>
+                </ScrollArea>
+              </div>
             </DialogDescription>
             <div className="flex justify-end mt-5">
               <Button disabled={!isTosAccepted} onClick={handleNextClick}>
