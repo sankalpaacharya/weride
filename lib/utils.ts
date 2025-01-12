25d6e7c12cb324b31d4320763afd49b95551f55d
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-01-12 11:49:01 +0530

    fix(date): fix the discord message data and tie

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { DISCORD_URL } from "@/utils/constants";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { format, toDate } from 'date-fns-tz';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -22,8 +23,10 @@ export async function sendDiscordMessage(userId: string, message: string) {
 }
 
 export function getFormatedDate() {
-  const date = new Date();
-  return `${date.toISOString().split("T")[0]}, ${date.toLocaleTimeString()}`;
+  const indiaTimezone = 'Asia/Kolkata';
+  const now = new Date();
+  const zonedDate = toDate(now, { timeZone: indiaTimezone });
+  return `${format(zonedDate, 'yyyy-MM-dd')}, ${format(zonedDate, 'hh:mm:ss a', { timeZone: indiaTimezone })}`;
 }
 
 export function discordRenterMessageMaker(
