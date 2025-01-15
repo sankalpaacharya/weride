dbc4039ad850f284749e3e2097c2333ab0e06b90
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-01-15 21:05:27 +0530

    fix(ui): props type fix

--- app/(weride)/(protected)/rental/request/page.tsx
+++ app/(weride)/(protected)/rental/request/page.tsx
@@ -1,28 +1,7 @@
 import React from "react";
 import RentalRequestCard from "@/components/rentalrequestcard";
-const RentalRequestDetails = {
-  user: {
-    name: "Sankalpa Acharya",
-    profileUrl: "https://github.com/sankalpaacharya", // storage me mil jayega
-    // url pattern: https://cxlnoycrdkdkdezryaph.supabase.co/storage/v1/object/public/Profile/{userId}.png
-  },
-  rideDetails: {
-    location: "bhaijipura",
-    duration: "4 hours",
-    distance: "10 KM",
-    amount: "₹120",
-    startTimeDate: "10:00am, 2024/1/17",
-    endTimeDate: "1:00pm, 2024/1/17",
-    timeleft: 600, // in seconds, createdtime tabale ke row me h aur estimated hours se endtime calculate karna, aur timeleft dono ka difference
-  },
-};
 
-type RentalRequestType = typeof RentalRequestDetails;
-type PageProps = {
-  rentalRequestDetails: RentalRequestType;
-};
-
-export default function Page({}: PageProps) {
+export default function Page() {
   return (
     <div>
       <RentalRequestCard />
--- lib/supabase/queries.ts
+++ lib/supabase/queries.ts
@@ -32,7 +32,6 @@ type VehicleInsert = {
   owner_name: string;
 };
 
-
 // in function in this file im passing arguments like an class object
 // function(this,this,this)
 // change it to function({this,this,this}) will be easy to work with
