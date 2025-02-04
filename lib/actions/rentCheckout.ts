de84f1ee20be2cfe83f576042bcb9636870b8a7d
Nishit Barbhaya <nishitbarbhaya06@gmail.com>
2025-02-04 19:23:44 +0530

    feat(email): send email to owner

--- lib/actions/rentCheckout.ts
+++ lib/actions/rentCheckout.ts
@@ -11,6 +11,7 @@ import {
   discordRenterMessageMaker,
   sendDiscordMessage,
 } from "../utils";
+import { sendMail } from '../../resend/mail';
 
 export async function rentCheckoutAction(data: TcheckOutSchema) {
   try {
@@ -32,12 +33,28 @@ export async function rentCheckoutAction(data: TcheckOutSchema) {
       location: data.location,
       rent_hour: data.hour,
     };
+    const { data: vehicleName, error: vehicleError} = await supabase.from('vehicle').select('name').eq('id',data.bikeId).single()
+    if (vehicleError){
+      console.error("Could not fetch vehicle",vehicleError);
+      return { error : "Could not fetch vehicle" }
+    }
+    const { data: ownerData, error:ownerError } = await supabase.from('users').select('email,name').eq('id',data.ownerId).single()
+    if (ownerError) {
+      console.error("Error fetching owner's email:", ownerError);
+      return { error: "Couldn't fetch owner's email" };
+    }
+    const ownerEmail = ownerData?.email;
+    const ownerName = ownerData?.name;    
+
     const { error } = await supabase.from("order").insert(insertData).select();
 
     if (error) {
-      return { error: "can't data add to the orders table" };
+      return { error: "can't add data to the orders table" };
     }
     const vehicleData = await updateVehicleStatus(data.bikeId, "Pending");
+
+    // send email to owner from weride 
+    await sendMail(ownerEmail, ownerName, rentUser.name, data.hour, data.location, rentUser.phone, vehicleName?.name );
     // send the discord message if user has discord id in db else skip this part
     await sendDiscordMessage(
       user?.id || "",
@@ -55,6 +72,9 @@ export async function rentCheckoutAction(data: TcheckOutSchema) {
         hours: data.hour,
       }),
     );
+
+
+
     if (!result.success) {
       return { error: result.error.issues[0].message };
     }
--- resend/mail.ts
+++ resend/mail.ts
@@ -2,11 +2,41 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const sendMail = async () => {
+export const sendMail = async (  ownerEmail: string,  
+  ownerName: string,  
+  renterName: string,  
+  hours: string,       
+  location: string,
+  phone : string,
+  bikeName : string
+) => {
   const { data, error } = await resend.emails.send({
     from: "Weride <team@weride.live>",
-    to: ["sankalp.ace22@sot.pdpu.ac.in"],
+    to: [ownerEmail],
     subject: "Request a Book",
-    html: "hello this is testing email",
+    html: `<div style="background: linear-gradient(to right, #1f2937, #374151); padding: 40px; color: #f3f4f6; min-height: 100vh; font-family: Arial, sans-serif;">
+  <h1 style="margin-bottom: 48px; text-align: center; font-size: 32px; font-weight: bold;">Rental Request</h1>
+  <div style="border-radius: 16px; background: rgba(167, 139, 250, 0.2); padding: 24px; font-family: monospace;">
+    <p style="padding-left: 28px; margin-top: 28px; text-align: left;">Hi <span style="font-weight: 800;">${ownerName},</span></p>
+    
+    <p style="padding-left: 28px; margin-top: 28px; text-align: left;">
+      <span style="font-weight: 800;">${renterName}</span> has requested to rent your vehicle 
+      <strong>${bikeName}</strong> for <strong>${hours}</strong> hours.
+    </p>
+    
+    <p style="padding-left: 28px; margin-top: 28px; text-align: left;">📍 Pickup Location: <strong>${location}</strong></p>
+    <p style="padding-left: 28px; margin-top: 8px; text-align: left;">📞 Renter’s Contact: <strong>${phone}</strong></p>
+    
+    <p style="padding-left: 28px; margin-top: 20px; text-align: left;">⏳ Please respond to this request within <strong>20 mins.</strong></p>
+    
+    <p style="padding-left: 28px; margin-top: 20px; text-align: left; color: #9ca3af; font-weight: 800;">Cheers,</p>
+    <p style="padding-left: 28px; text-align: left; margin-bottom: 28px; color: #9ca3af; font-weight: 800;">Team Weride !!</p>
+  </div>
+</div>`,
   });
+  if (error) {
+    console.error("Email Sending Failed: ");
+    return {error};
+  }
+  return {success : "Email sent successfully",data};
 };
